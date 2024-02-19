b06236371982f128a7398b1322a5b5e98b9cd129
Chrapson <marcin.chraplewski@gmail.com>
2024-02-19 13:38:26 +0100

    h1 fix

--- src/ui/atoms/ProductListItemDesc.tsx
+++ src/ui/atoms/ProductListItemDesc.tsx
@@ -10,7 +10,6 @@ export const ProductListItemDesc = ({
 }: ProductListItemDescProps) => {
 	return (
 		<div className="flex flex-col items-center justify-center">
-			<p></p>
 			<p>{name}</p>
 			<p>Category: {category}</p>
 			<p>Price: {price} $</p>
