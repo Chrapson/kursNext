4b9a547c456a40ca2b394ecdf5967f73483ffd34
Chrapson <marcin.chraplewski@gmail.com>
2024-02-19 13:16:01 +0100

    test timeout update

--- __checks__/custom.spec.ts
+++ __checks__/custom.spec.ts
@@ -1,10 +1,10 @@
-import { test, expect } from '@playwright/test'
+import { test, expect } from "@playwright/test";
 
 // You can override the default Playwright test timeout of 30s
-// test.setTimeout(60_000);
+test.setTimeout(15000);
 
-test('Custom Browser Check', async ({ page }) => {
-  const response = await page.goto('https://kurs-next-lake.vercel.app/')
-  expect(response?.status()).toBeLessThan(400)
-  await page.screenshot({ path: 'screenshot.jpg' })
-})
+test("Custom Browser Check", async ({ page }) => {
+	const response = await page.goto("https://kurs-next-lake.vercel.app/");
+	expect(response?.status()).toBeLessThan(400);
+	await page.screenshot({ path: "screenshot.jpg" });
+});
